1ed2cc90a7a9755e3dd140b41d0c2cafc3c71b0c
rafael ferraz <rferraz_21@hotmail.com>
2020-08-19 12:50:32 -0300

    Add restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#puts "Creating restaurants..."
+
+#10.times do
+#  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, stars: rand(1..5), chef_name: Faker::Name.name_with_middle)
+#end
